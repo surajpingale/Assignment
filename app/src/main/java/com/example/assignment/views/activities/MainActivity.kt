d7d6068bd14daa7b00e35eb655dc2b03f8305b9a
Suraj Pingale <surajpingale02@gmail.com>
2022-08-11 19:39:30 +0530

    changes

--- app/src/main/java/com/example/assignment/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/assignment/views/activities/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.example.assignment.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.assignment.R
+import com.example.assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id)
+                as NavHostFragment
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragment_products,
+                R.id.fragment_add_product
+            )
+        )
+        setupActionBarWithNavController(navHost.navController, appBarConfiguration)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
